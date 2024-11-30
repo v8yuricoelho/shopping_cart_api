c6c3f3671ecacff71c2f2565a2a095f57f52c9f1
Yuri <ycr.data@gmail.com>
2024-11-30 00:01:09 -0300

    implementing cart total price udpating logic

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
 class CartItem < ApplicationRecord
+  after_save :update_cart_total_price
+  after_destroy :update_cart_total_price
+
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+  validates :product, presence: true
+  validates :cart, presence: true
+
   belongs_to :cart
   belongs_to :product
 
-  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
-
   def total_price
-    quantity * product.price.to_f.round(2)
+    (quantity * product.price.to_f).round(2)
+  end
+
+  private
+
+  def update_cart_total_price
+    total = cart.cart_items.joins(:product).sum('cart_items.quantity * products.price')
+    cart.update_column(total_price: total)
   end
 end
